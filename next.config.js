400199eba7a0cc9711f4558e8f1f0a5043fa9448
Hiroshi Urabe <mail@torounit.com>
2020-11-24 02:46:33 +0900

    add sw.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const settings = {
     WORDPRESS_URL: process.env.WORDPRESS_URL || 'http://localhost:12345',
     APP_ID : 'bf88fce4-864e-44d8-80f1-ce6758381473',
     APP_NAME: 'next',
+    pwa: {
+      dest: 'public'
+    }
   }
 }
 module.exports = process.env.NODE_ENV !== 'production' ? settings: withPWA(settings)
