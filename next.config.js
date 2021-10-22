10868d43dcfcfb0e2e6b9b6f9e95aba991d28e55
Hiroshi Urabe <mail@torounit.com>
2021-10-22 18:20:03 +0900

    update next.11

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const withPWA = require('next-pwa')
 const settings = {
   env: {
     WORDPRESS_URL: process.env.WORDPRESS_URL || 'http://localhost:12345',
     APP_ID : 'bf88fce4-864e-44d8-80f1-ce6758381473',
     APP_NAME: 'next',
-    pwa: {
-      dest: 'public'
-    }
   }
 }
-module.exports = process.env.NODE_ENV !== 'production' ? settings: withPWA(settings)
+module.exports = settings;
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,12 @@ import Head from 'next/head';
 import useSWR from 'swr';
 import { fetcher } from '../lib/apiFetch';
 import Link from 'next/link';
-import { useEffect } from 'react';
 
 export default function Home( { posts: initialData } ) {
   const { data: posts, error } = useSWR( '/wp/v2/posts?per_page=-1', fetcher, { initialData } );
+  console.log( posts );
   if ( error ) return <div>failed to load</div>;
 
-  const user = useSWR( '/wp/v2/users/me', fetcher );
-  useEffect( () => {
-    fetcher( '/wp/v2/users/me' ).then( (result) => console.log(result))
-  })
   return (
     <div>
       <Head>
@@ -31,7 +27,7 @@ export default function Home( { posts: initialData } ) {
 }
 
 export const getStaticProps = async () => {
-  const posts = await fetcher( '/wp/v2/posts?per_page=-1' );
+  const posts = await fetcher( '/wp/v2/posts?per_page=-1' )
   return {
     props: {
       posts: posts || []
